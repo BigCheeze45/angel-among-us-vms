757caf3443b657b7a914c6b2c836c8fd0d4a51f4
Prince Duepa <kannahprince12@gmail.com>
2024-09-21 14:59:33 -0400

    Add DRF default filtering backend

--- vms/settings.py
+++ vms/settings.py
@@ -40,6 +40,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
     ],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 25,
 }
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
